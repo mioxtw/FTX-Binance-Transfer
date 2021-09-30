defa8cad1493ab467d3119c4da1c6e94267b824e
Mio Su <miox.tw@gmail.com>
2021-09-30 12:54:02 +0800

    v0.3
    Fix Binance to Fix bug

--- B2F.py
+++ B2F.py
@@ -7,7 +7,7 @@ import math
 
 
 print("--------------------------------------------------------------------")
-print("Binance to Ftx v0.2")
+print("Binance to Ftx v0.3")
 print("--------------------------------------------------------------------")
 print("")
 print("")
@@ -24,20 +24,25 @@ ftx_busd_bsc_address = data['ftx-busd-bsc-address']
 binance_busd_bsc_address = data['binance-busd-bsc-address']
 
 
-if float(sys.argv[1]) < 10:
+size = float(sys.argv[1])
+
+
+if size < 10:
     print(f"幣安提現BUSD最小數量為10")
     sys.exit()
 
 binance = Client(BapiKey,BapiSecret)
-transfer_status = binance.make_universal_transfer(type='UMFUTURE_MAIN', asset='BUSD', amount=sys.argv[1])
+transfer_status = binance.make_universal_transfer(type='UMFUTURE_MAIN', asset='BUSD', amount=size)
 #print(transfer_status)
-print(f"[Binance] U本位帳戶劃轉 {sys.argv[1]} BUSD 到現貨帳戶")
+print(f"[Binance] U本位帳戶劃轉 {size} BUSD 到現貨帳戶")
+
+time.sleep(1)
 
 while True:
     balance = binance.get_asset_balance('BUSD')
     if float(balance['free']) != 0:
-        binance.withdraw(coin='BUSD',network='BSC', address=ftx_busd_bsc_address, amount=sys.argv[1])
-        print("[Binance] Sending "+ sys.argv[1] + " BUSD now")
+        binance.withdraw(coin='BUSD',network='BSC', address=ftx_busd_bsc_address, amount=size)
+        print(f"[Binance] Sending {size} BUSD now")
         break;
     time.sleep(1)
 
